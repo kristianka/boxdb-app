a2e364c0b4b8d90c0962b99c7032c17ad57dbd90
Kristian <49764796+kristianka@users.noreply.github.com>
2024-06-04 15:09:08 +0300

    fix lint issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// prevents infinite loops with useEffects
 import "./App.css";
 import "./i18n/config";
 import { useEffect, useState } from "react";
