9da15417707ee64421c40891d215fdcb9223b423
Kristian <49764796+kristianka@users.noreply.github.com>
2024-06-14 11:17:51 +0300

    remove unnecessary ports, add some cooldown for slow machines

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Wait for 30 seconds to ensure the database is ready
+echo "Waiting for 30 seconds to ensure the database is ready..."
+sleep 30
+
 # migrate database
 npx prisma generate
 npx prisma migrate dev --name init
